c7f5cd57e6adfefde0e40827a1f9779fba705492
Asaiy420 <kcaryan05@gmail.com>
2025-05-14 19:49:22 +0545

    added user route and controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,20 @@
-import express, {Request, Response} from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import {User} from '../models/user.model.js';
 
+export const getAllUsers = async (req: Request, res:Response, next: NextFunction): Promise<any> => {
+    try{
+        const currentUserId = req.auth?.userId;
+        const users = await User.find({clerkId: {$ne: currentUserId}});    //exclude current user
+        
 
-export const Users = async (req: Request, res:Response) => {
-    res.send("Users Controller is working");
+        if (!users){
+            return res.status(404).json({message: "No users found"});
+        }
+
+        return res.status(200).json(users);
+    }catch(error:any){
+        console.log("Error in getAllUsers controller", error.message);
+        next(error);
+
+    }
 }
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { Users } from "../controllers/user.controller.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/users", Users);
+router.get("/users", getAllUsers);
 
 export default router;
