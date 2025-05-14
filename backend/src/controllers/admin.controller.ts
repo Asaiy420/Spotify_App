b0cd2a035f2c1a325916bb403ce97d0ff509a11c
Asaiy420 <kcaryan05@gmail.com>
2025-05-14 13:43:52 +0545

    added admin contoller for logic and album routes

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -3,6 +3,19 @@ import { Song } from "../models/song.model.js";
 import { Album } from "../models/album.model.js";
 import cloudinary from "../lib/cloudinary.js";
 
+export const checkAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    return res.status(200).json({ Admin: true });
+  } catch (error: any) {
+    console.error("Error checking admin:", error.message);
+    next(error);
+  }
+};
+
 // helper function to upload files to Cloudinary
 
 const uploadToCloudinary = async (file: any) => {
@@ -13,7 +26,7 @@ const uploadToCloudinary = async (file: any) => {
     return result.secure_url; // Return the secure URL of the uploaded file
   } catch (error: any) {
     console.error("Error uploading to Cloudinary:", error.message);
-    throw new Error("Failed to upload file to Cloudinary"); 
+    throw new Error("Failed to upload file to Cloudinary");
   }
 };
 
@@ -47,7 +60,7 @@ export const createSong = async (
     // If an albumId is provided, update the album to include the new song
     if (albumId) {
       await Album.findByIdAndUpdate(albumId, {
-        $push: { songs: song._id },
+        $push: { songs: song._id }, // Add the song ID to the album's songs array
       });
     }
 
@@ -55,5 +68,88 @@ export const createSong = async (
   } catch (error: any) {
     console.error("Error creating song:", error.message);
     next(error); // Pass the error to the next middleware
+  }
+};
+
+export const deleteSong = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    const song = await Song.findById(id);
+
+    // check if song belongs to an album
+
+    if (song?.albumId) {
+      await Album.findByIdAndUpdate(song.albumId, {
+        $pull: { songs: song._id }, // Remove the song ID from the album's songs array
+      });
+    }
+    await Song.findByIdAndDelete(id);
+
+    return res.status(200).json({ message: "Song deleted successfully" });
+  } catch (error: any) {
+    console.error("Error deleting song:", error.message);
+    next(error);
+  }
+};
+
+export const createAlbum = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    if (!req.files || !req.files.imageFile) {
+      return res.status(400).json({ error: "Image file is required" });
+    }
+
+    const { title, artist, releaseYear } = req.body;
+
+    const { imageFile } = req.files;
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    const album = new Album({
+      title,
+      artist,
+      imageUrl,
+      releaseYear,
+    });
+
+    await album.save();
+    return res.status(201).json(album);
+  } catch (error: any) {
+    console.error("Error creating album:", error.message);
+    next(error);
+  }
+};
+
+export const deleteAlbum = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    const album = await Album.findById(id);
+
+    if (!album) {
+      return res.status(404).json({ error: "Album not found" });
+    }
+
+    // delete all songs associated with the album
+    if (album.songs.length > 0) {
+      await Song.deleteMany({ _id: { $in: album.songs } });
+    }
+    await Album.findByIdAndDelete(id);
+
+    return res.status(200).json({ message: "Album deleted successfully" });
+  } catch (error: any) {
+    console.error("Error deleting album:", error.message);
+    next(error);
   }
 };
--- backend/src/controllers/album.controller.ts
+++ backend/src/controllers/album.controller.ts
@@ -1,5 +1,42 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { Album } from "../models/album.model.js";
 
-export const Albums = async (req: Request, res: Response) => {
-  res.send("Albums Controller is working");
+export const getAllAlbums = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const albums = await Album.find();
+
+    if (!albums) {
+      return res.status(404).json({ message: "No albums found" });
+    }
+
+    return res.status(200).json(albums);
+  } catch (error: any) {
+    console.error("Error fetching albums:", error.message);
+    next(error);
+  }
+};
+
+export const getAlbumById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { albumId } = req.params;
+ // populate the songs array with the song details
+    const album = await Album.findById(albumId).populate("songs"); 
+
+    if (!album) {
+      return res.status(404).json({ message: "Album not found" });
+    }
+
+    return res.status(200).json(album);
+  } catch (error: any) {
+    console.error("Error fetching album:", error.message);
+    next(error);
+  }
 };
